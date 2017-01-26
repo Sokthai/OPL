d3a7a45a5c683d6d2a3cee9915506390a7b3e35d
sokthai <sokthaitang@gmail.com>
2017-01-26 01:17:32 -0500

    push assignments

---
+++ ps0/ps0/ps0.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+;; exercise 1: procedure to square its argument
+;; make sure to use the lambda formulation
+(define square (lambda (n) (* n n))) 
+
+;; exercise 2: discussion group
+;; Sign up up for the course discussion group and find the value
+;; for the symbol secret, and copy it into the expression below.
+(define secret 'opl-is-fun)
