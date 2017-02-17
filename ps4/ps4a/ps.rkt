8a4dfb62d43189e3220b94e8131f41af96b36b86
sokthai <sokthaitang@gmail.com>
2017-02-17 12:13:12 -0500

    finish ps4a except the drawing

--- ps4/ps4a/ps.rkt
+++ ps4/ps4a/ps.rkt
@@ -46,8 +46,17 @@
 ;; Include as ASCII drawing in this file, or upload image named
 ;; exercise3.2-env.png (or .jpg)
 
-(define (make-monitored)
-  'foo)
+(define (make-monitored p)
+  (let ([count 0])
+    (lambda (input)
+      (cond
+        [(eq? input 'reset-count) (set! count 0)]
+        [(eq? input 'how-many-calls) count]
+        [else (set! count (+ count 1))
+                    (p input)])
+      )
+    )
+  )
 
 (define exercise-3.2-env #f)
 ;; change to #t after you include diagram
@@ -55,8 +64,23 @@
 
 ;; Exercise 3.3
 ;; https://mitpress.mit.edu/sicp/full-text/book/book-Z-H-20.html#%_thm_3.3
-(define (make-account)
-  'foo)
+(define (make-account balance passwd)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount)) balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch passwdVerify m)
+    (cond
+      [(not (eq? passwd passwdVerify)) (error "Incorrect password")]
+      [(eq? m 'withdraw) withdraw]
+      [(eq? m 'deposit) deposit]
+      [else (error "Unknown request: MAKE-ACCOUNT" m)]))
+
+  dispatch   
+  )
                                        
 ;; Exercise 3.4
 ;; https://mitpress.mit.edu/sicp/full-text/book/book-Z-H-20.html#%_thm_3.4
@@ -66,8 +90,26 @@
 ;; important note:
 ;;   once an account is locked, you should not be able to withdraw funds
 ;;   from it, even if then using the correct password!
-(define (make-account-secure)
-  'foo)
+(define (make-account-secure balance passwd)
+  (let ([attempt 0])
+    (define (withdraw amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount)) balance)
+          "Insufficient funds"))
+    (define (deposit amount)
+      (set! balance (+ balance amount))
+      balance)
+    (define (dispatch passwdVerify m)
+      (cond
+        [(>= attempt 7) (error (call-the-cops))]
+        [(not (eq? passwd passwdVerify))
+         (begin (set! attempt (+ attempt 1)) (error "Incorrect password"))]
+        [(eq? m 'withdraw) withdraw]
+        [(eq? m 'deposit) deposit]
+        [else (error "Unknown request: MAKE-ACCOUNT" m)]))
+    
+  dispatch   )
+  )
   
 ; don't modify this
 (define (call-the-cops)
