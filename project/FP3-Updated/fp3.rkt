d45388ecadfef5493c618afa085f0e7292e54652
sokthai <sokthaitang@gmail.com>
2017-04-12 11:46:31 -0400

    add project

--- project/FP3-Updated/fp3.rkt
+++ project/FP3-Updated/fp3.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require rsound)
+(require rsound net/sendurl)
 ;(require racket/gui)
 ;(require  json  net/url)
 (require  json srfi/13  net/url (only-in racket/gui play-sound))
@@ -18,16 +18,21 @@
 
 (define myrespond "")
 
-
-
+(define file-path "C:\\Users\\sokthai\\Downloads/")
+ (define sound-url "")
 
 (define (search w)
+
   
+ 
   (define con-url (string->url (string-append open_api w)))
   (define dict-port (get-pure-port con-url (list app_id app_key)))
   (define respond (port->string dict-port))
+
+
   
-  (begin (set! myrespond respond))
+  
+  
   
   (close-input-port dict-port)
   
@@ -36,9 +41,12 @@
          (searchDict (readjson-from-input respond) '|word| "word        : ")
          (searchDict (readjson-from-input respond) '|definitions| "definitions : ")
          (searchDict (readjson-from-input respond) '|examples| "examples    : ")
-         (searchDict (readjson-from-input respond) '|audioFile| "pronunciation : ")
+         (begin (set! sound-url (searchDict (readjson-from-input respond) '|audioFile| "pronunciation : ")))
+         (begin (set! myrespond respond))
   ))
-
+ 
+  (set! file-path (string-append file-path w "_gb_1.mp3"))
+  ;(send-url "http://fsda")
 )
 
 
@@ -61,17 +69,42 @@
                   (searchDict (hash-ref hash key) k des)))                  
                (else hash)))))
 
+;(define (display hash k des)
+;  (cond
+;    
+;    ((list? (hash-ref hash k))
+;     (cond
+;       ((string? (car (hash-ref hash k))) (printf "~a~a\n" des (car (hash-ref hash k))))
+;       (else
+;        (show (hash-ref hash k) k des))))
+;    (else (printf "~a~a\n" des (hash-ref hash k (lambda () ""))))
+;  ))
+
+
+
 (define (display hash k des)
   (cond
     
     ((list? (hash-ref hash k))
      (cond
-       ((string? (car (hash-ref hash k))) (printf "~a~a\n" des (car (hash-ref hash k))))
+       ((string? (car (hash-ref hash k))) (return des (car (hash-ref hash k))))
        (else
         (show (hash-ref hash k) k des))))
-    (else (printf "~a~a\n" des (hash-ref hash k (lambda () ""))))
+    (else (return des (hash-ref hash k (lambda () ""))))
   ))
-    
+
+(define (return des content result count )
+  (if (> count 0)
+      (if (list? result)
+          (return des content (append result (list content)) (- count 1))
+          (return des content (list result) (- count 1)))
+     
+      result )
+  ;(printf "~a~a\n return" des content)
+
+ 
+  ;"okay"
+)
   
 
 
@@ -80,7 +113,9 @@
   (cond ((null? lst) lst)
         (else
          (for (((key val) (in-hash (car lst )))) 
-           (printf "~a~a\n" des val ))
+           (printf "~a~a\n show" des val )
+           ;(return des val)
+           )
 
          (show (cdr lst) k des)
          )))
@@ -99,13 +134,13 @@
 
 
 ;-----play mp3
-as
-dsf
-fdfsfdfsa
-(define pth "hello.mp3")    ; path to audio file (windows can only play .wav)
+
+(define pth "./hello.mp3")    ; path to audio file (windows can only play .wav)
 
 (define play-asynchronously #t) ; continue execution while file is playing?
 
-(define play (play-sound pth play-asynchronously))
+;(define play (play-sound pth play-asynchronously))
 
-(define p "http://audio.oxforddictionaries.com/en/mp3/angry_gb_1.mp3")
+
+
+;(define p "http://audio.oxforddictionaries.com/en/mp3/angry_gb_1.mp3")
