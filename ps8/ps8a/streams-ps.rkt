5a181a845704dafc059f1bd7d1e9010971f43bab
sokthai <sokthaitang@gmail.com>
2017-04-13 09:48:41 -0400

    add ps8

---
+++ ps8/ps8a/streams-ps.rkt
@@ -0,0 +1,237 @@
+#lang racket
+
+;;; +++++++++ Required for auto grading ++++++++++++
+
+;; Allow name nil to be used for the empty list.
+(define nil '()) 			
+
+;; Some other constants with non-traditional names:
+(define true #t)
+(define false #f)
+
+;; For stream programming section 3.5
+;; We use a force and delay which are slightly
+;; different from the force and delay defined in the book:
+;; delay (aka make-promise) handles recursive forcing.
+
+(define the-empty-stream '())
+(define stream-null? null?)
+(define stream-car car)
+(define (stream-cdr s)
+  (force (cdr s)))
+
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ head tail)
+     (cons head (delay tail)))))
+
+(define (stream-ref a n)
+  (if (= n 0)
+      (stream-car a)
+      (stream-ref (stream-cdr a) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+; Following is the general stream-map from Exercise 3.50
+(define (stream-map-general proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream 
+       (apply proc (map stream-car argstreams))
+       (apply stream-map-general (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (add-streams s1 s2)
+  (stream-map-general + s1 s2))
+
+(define (stream->list stream)
+            (if (stream-null? stream)
+                '()
+                (cons (stream-car stream)
+                      (stream->list (stream-cdr stream)))))
+
+;;; *******************************************************************
+;;; Use stream-map (pp. 320 of textbook) to define a procedure called
+;;; convert-temps that takes a stream of temperatures in Fahrenheit
+;;; and returns a stream of converted temperatures in Celsius. Recall
+;;; that to convert Fahrenheit to Celsius, the equation is C = 5/9 *
+;;; (F-32).
+
+;; first define a proc named convert-f-to-c which converts a single value
+
+(define (convert-f-to-c f)
+  f)
+
+;; then use that function with stream-map to define convert-temps
+
+(define (convert-temps fahrenheit-stream)
+  fahrenheit-stream)
+
+
+;;; ******************************************************************
+;;; Use stream-filter (pp. 322 of textbook) to define the stream of
+;;; all integers that are evenly divisible by 2, 3, or 5.
+
+(define (divisible? x y)
+  (= (remainder x y) 0))
+
+(define (divisible-by-2-3-5 int-stream)
+  int-stream)
+
+;;; ******************************************************************
+;;; Replace the lambda function to complete the following alternative
+;;; definition of the integers stream:
+;;;
+;;; (define my-integers (cons-stream 1 (stream-map <??> my-integers)))
+
+(define my-integers
+  (cons-stream 1 (stream-map (lambda (x) x) my-integers)))
+
+ 
+;;; ******************************************************************
+;;; This is based on Exercise 3.51 on pp. 325:
+;;; http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-24.html#%_thm_3.51
+;;;
+;;; NOTE:
+;;; YOU SHOULD TRY TO ANSWER THIS WITHOUT RUNNING THE CODE.
+;;; questions like this will be on the final exam.
+;;;
+;;; In order to take a closer look at delayed evaluation, we will use
+;;; the following procedure, which simply returns its argument after
+;;; printing it:
+;;;
+(define (show x)
+  (display-line x)
+  x)
+;;;
+;;; Answer the following questions by changing the values in the associated
+;;; definitions.
+;;; 
+;;; After evaluating the following statement:
+;;; (define x (stream-map show (stream-enumerate-interval 0 10)))
+;;;
+;;; How many cons cells in stream x exist?
+(define ex351-num-cons-cells 0)
+
+;;; Of these cons cells in stream x, how many have values in their car positions
+;;; that have already been forced (or were never delayed in the first place)?
+(define ex351-num-forced-at-outset 0)
+;;;
+;;; Now evaluate this expression:
+;;; (stream-ref x 5)
+;;;
+;;; How many cons cells exist now?
+(define ex351-num-cons-cells-after-stream-ref-5 0)
+;;;
+;;; How many car-positions have been forced now?
+(define ex351-num-forced-after-stream-ref-5 0)
+;;;
+;;; If "(stream-ref x 5)" is evaluated again, will the either of the two
+;;; answers above change?
+;;; Change the 0 to 'yes or 'no.
+(define ex351-num-forced-twice-stream-ref-5 0)
+;;;
+;;; Now evaluate:
+;;; (stream-ref x 7)
+;;;
+;;; How many car-positions have been forced now?
+(define ex351-num-forced-after-stream-ref-7 0)
+;;;
+;;; How many total cons cells have been created at this point?
+;;; Hint: recognize that two streams exist -- the original stream
+;;; created by stream-enumerate-interval, and the stream created
+;;; by stream-map.
+(define ex351-total-cons-cells 0)
+
+
+;;; ******************************************************************
+;;; This is based on Exercise 3.52:
+;;; http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-24.html#%_thm_3.52
+;;;
+;;; NOTE:
+;;; YOU SHOULD TRY TO ANSWER THIS WITHOUT RUNNING THE CODE.
+;;; questions like this will be on the final exam.
+;;;
+;;; Or at least, if you run the code to get the answers,
+;;; make sure you understand why you are getting the results that you do.
+;;;
+;;; You should manually calculate each value of the "seq" stream.
+;;; 
+;;; Consider this sequence of expressions:
+(define sum 0)
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+;;;
+;;; After these expresssions are evaluated,
+;;; what is the value of sum?
+(define ex352-sum-initial-value 0)
+;;;
+;;; Now we evaluate:
+;;; (define y (stream-filter even? seq))
+;;; How many cons cells does seq have after this definition?
+(define ex352-num-cons-seq-after-y-defined 0)
+;;; What is the value of sum after y is defined?
+(define ex352-sum-after-y-defined 0)
+;;;
+;;; Now we evaluate:
+;;; (define z (stream-filter (lambda (x) (= (remainder x 5) 0)) seq))
+;;; How many cons cells does seq have after this definition?
+(define ex352-num-cons-seq-after-z-defined 0)
+;;; What is the value of sum after z is defined?
+(define ex352-sum-after-z-defined 0)
+;;;
+;;; Now we evaluate:
+;;; (stream-ref y 2)
+;;; How many cons cells does y have?
+(define ex352-num-cons-y-after-stream-ref-y-2 0)
+;;; How many cons cells does seq have?
+(define ex352-num-cons-seq-after-stream-ref-y-2 0)
+;;; What is sum?
+(define ex352-sum-after-stream-ref-y-2 0)
+;;;
+;;; Now we evaluate:
+;;; (display-stream z)
+;;; How many cons cells does seq have?
+(define ex352-num-cons-seq-after-display-z 0)
+;;; How many cons cells does z have?
+(define ex352-num-cons-z-after-display-z 0)
+
+;;; ************************ END OF FILE *****************************
---
+++ ps8/ps8b/streams-ps.rkt
@@ -0,0 +1,195 @@
+#lang racket
+
+;;; +++++++++ Required for auto grading ++++++++++++
+
+;; Allow name nil to be used for the empty list.
+(define nil '()) 			
+
+;; Some other constants with non-traditional names:
+(define true #t)
+(define false #f)
+
+;; For stream programming section 3.5
+;; We use a force and delay which are slightly
+;; different from the force and delay defined in the book:
+;; delay (aka make-promise) handles recursive forcing.
+
+(define the-empty-stream '())
+(define stream-null? null?)
+(define stream-car car)
+(define (stream-cdr s)
+  (force (cdr s)))
+
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ head tail)
+     (cons head (delay tail)))))
+
+(define (stream-ref a n)
+  (if (= n 0)
+      (stream-car a)
+      (stream-ref (stream-cdr a) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+; Following is the general stream-map from Exercise 3.50
+(define (stream-map-general proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream 
+       (apply proc (map stream-car argstreams))
+       (apply stream-map-general (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (add-streams s1 s2)
+  (stream-map-general + s1 s2))
+
+(define (stream->list stream)
+            (if (stream-null? stream)
+                '()
+                (cons (stream-car stream)
+                      (stream->list (stream-cdr stream)))))
+
+
+;;; ******************************************************************
+;;; Based Exercise 3.53 on pp. 330:
+;;; http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-24.html#%_thm_3.53
+;;;
+;;; YOU SHOULD TRY TO ANSWER THIS WITHOUT RUNNING THE CODE.
+;;; Questions like this will be on the final exam.
+;;;
+;;; Consider this stream:
+(define s (cons-stream 1 (add-streams s s)))
+;;;
+;;; What is:
+;;; (stream-ref s 0) ?
+(define ex353-s-0 0)
+;;;
+;;; What is (stream-ref s 1) ?
+(define ex353-s-1 0)
+;;;
+;;; What is (stream-ref s 2) ?
+(define ex353-s-2 0)
+;;;
+;;; What is (stream-ref s 3) ?
+(define ex353-s-3 0)
+
+
+;;; ******************************************************************
+;;; Complete Exercise 3.54 on pp. 331:
+;;; http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-24.html#%_thm_3.54
+;;;
+;;; Define a procedure mul-streams, analogous to add-streams, that
+;;; produces the elementwise product of its two input streams. Use
+;;; this together with the stream of integers to complete the
+;;; following definition of the stream whose nth element (counting
+;;; from 0) is n + 1 factorial:
+;;;
+;;; (define factorials (cons-stream 1 (mul-streams <??> <??>)))
+
+(define (mul-streams s1 s2)  ;; this need to be implemented
+  s1)
+
+(define factorials 
+  (cons-stream 1 (mul-streams integers integers))) ;; this line needs to be changed
+
+
+;;; ******************************************************************
+;;; This is based on Exercise 3.57:
+;;; https://mitpress.mit.edu/sicp/full-text/book/book-Z-H-24.html#%_thm_3.57
+;;;
+;;; Consider the definition of Fibonacci using add-streams:
+(define fibs
+  (cons-stream 0
+               (cons-stream 1
+                            (add-streams (stream-cdr fibs)
+                                         fibs))))
+;;; remember that add-streams is:
+;;; (define (add-streams s1 s2)
+;;;   (stream-map + s1 s2))
+;;;
+;;; This is 0-indexed Fibonacci starting at 0, so:
+;;; (stream-ref fibs 0) => 0 (0th Fibonacci number is 0) - base case
+;;; (stream-ref fibs 1) => 1 (1st Fib # is 1) - base case
+;;; (stream-ref fibs 2) => 1 (2nd Fib # is 1)
+;;; (stream-ref fibs 3) => 2 (3rd Fib # is 2)
+;;; (stream-ref fibs 4) => 3 (4th Fib # is 3)
+;;; (stream-ref fibs 5) => 5 (5th Fib # is 5)
+;;;
+;;; Remember that streams memoize previously computed values.
+;;; How many additions are involved in calculating (stream-ref fibs 2)?
+;;; change the 0 to the correct answer.
+(define fib-2-with-memo 0)
+;;;
+;;; How many additions to calculate (stream-ref fibs 3)?
+(define fib-3-with-memo 0)
+;;;
+;;; How many additions to calculate (stream-ref fibs 4)?
+(define fib-4-with-memo 0)
+;;;
+;;; Write a function that calculates how many additions are necessary
+;;; to calculate the nth Fibonacci number. Remember that there are no
+;;; additions necessary for the two base cases of the 0th and 1st Fibonacci
+;;; number.
+(define (fib-num-adds-with-memo n)
+  0)
+;;;
+;;; Now suppose that streams did NOT memoize previously-computed values;
+;;; e.g., that (delay <exp>) were simply implemented as (lambda () <exp>).
+;;; Now what is the story?
+;;;
+;;; How many additions for (stream-ref fibs 2) ?
+(define fib-2-wo-memo 0)
+;;;
+;;; for (stream-ref fibs 3) ?
+(define fib-3-wo-memo 0)
+;;;
+;;; for (stream-ref fibs 4) ?
+(define fib-4-wo-memo 0)
+;;;
+;;; for (stream-ref fibs 5) ?
+(define fib-5-wo-memo 0)
+;;;
+;;; Write a function that calculates how many additions will be performed
+;;; in computing the nth Fibonacci number in the case where streams do not
+;;; memoize.
+(define (fib-num-adds-wo-memo n)
+  0)
+
+;;; ************************ END OF FILE *****************************
